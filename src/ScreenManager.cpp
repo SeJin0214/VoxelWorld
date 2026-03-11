7cc64e8459280878ee3d5e7f50fc5957b843626d
SeJin0214 <se.jin0214a@gmail.com>
2026-03-12 00:04:24 +0900

    fix: 풀스크린으로 동작하도록 변경

--- src/ScreenManager.cpp
+++ src/ScreenManager.cpp
@@ -5,8 +5,10 @@ ScreenManager ScreenManager::instance;
 
 void ScreenManager::CreateHWND(WCHAR windowClass[], WCHAR title[], HINSTANCE hInstance)
 {
-	mHwnd = CreateWindowExW(0, windowClass, title, WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, WIDTH, HEIGHT,
+	uint32_t screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	uint32_t screenHeight = GetSystemMetrics(SM_CYSCREEN);
+	mHwnd = CreateWindowExW(0, windowClass, title, WS_POPUP | WS_VISIBLE,
+		CW_USEDEFAULT, CW_USEDEFAULT, screenWidth, screenHeight,
 		nullptr, nullptr, hInstance, nullptr);
 	assert(mHwnd != nullptr);
 }
